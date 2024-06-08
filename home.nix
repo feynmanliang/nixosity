043e78cf3053d0d52670cb328ef9a247d87162b6
Feynman Liang <feynman.liang@gmail.com>
2024-06-08 16:26:26 +0000

    Git and CLI

--- home.nix
+++ home.nix
@@ -1,20 +1,37 @@
 { pkgs, ... }: {
+  home.username = "nixos";
+  home.homeDirectory = "/home/nixos";
+
   home.packages = [
     pkgs.git
     pkgs.tmux
     pkgs.neovim
 
     pkgs.nixpkgs-fmt
+
+    pkgs.jq
+    pkgs.yq-go
+
+    pkgs.btop
+    pkgs.dnsutils
   ];
 
   programs = {
+    bash.enable = true;
+
     direnv = {
       enable = true;
       enableBashIntegration = true;
       nix-direnv.enable = true;
     };
 
-    bash.enable = true;
+    git = {
+      enable = true;
+      userName = "Feynman Liang";
+      userEmail = "feynman.liang@gmail.com";
+    };
+
+    starship.enable = true;
   };
 
   # The state version is required and should stay at the version you
