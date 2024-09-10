5a0373c289e115df0a95293efdec1bdfbbb2dc5a
Feynman Liang <feynman@blueteam.ai>
2024-09-10 07:50:59 -0700

    Switch gotodef to finder

--- home-manager/my_nvchad_config/lua/mappings.lua
+++ home-manager/my_nvchad_config/lua/mappings.lua
@@ -7,6 +7,6 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "<leader>fW", "<cmd> Telescope grep_string <cr>")
-map("n", "<leader>gd", "<cmd> Lspsaga goto_definition <cr>")
+map("n", "<leader>gd", "<cmd> Lspsaga finder <cr>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
